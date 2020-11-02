ccf47201679feff124988b8a299dc5d7cf71f8a3
Vadim Kachevski <vadimkache@gmail.com>
2020-11-02 12:49:36 +0200

    game upload

---
+++ Assets/Scripts/MovePlayer1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovePlayer1 : MonoBehaviour
+
+{
+    private SpriteRenderer SR;
+    private float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if ( Input.GetKey(KeyCode.LeftArrow))
+        {
+                transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+                SR.flipX = true;
+            
+        }
+        if ( Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            SR.flipX = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MovePlayer2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    private SpriteRenderer SR;
+    private float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            SR.flipX = true;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            SR.flipX = false;
+        }
+    }
+}
